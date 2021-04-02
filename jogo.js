33afd6c042ebd6145d58f62fbe9faad12bb03fe0
RuanSalo <ruansalo03@gmail.com>
2021-04-02 18:40:22 -0300

    Tela de cadastro personalizada,e quase funcional

--- jogo.js
+++ jogo.js
@@ -9,6 +9,14 @@ const elementos = {
   teclado: document.querySelector('.teclado'),
   palavra: document.querySelector('.palavra'),
   dica: document.querySelector('.dica .pista'),
+  formulario: {
+    facil: document.getElementById('radioFacil'),
+    medio: document.getElementById('radioMedio'),
+    dificil: document.getElementById('radioDificil'),
+    palavra: document.getElementById('palavraCadastro'),
+    dica: document.getElementById('dicaCadastro'),
+    botaoCadastro: document.getElementById('botaoCadastro'),
+  },
   botoes: {
     facil: document.querySelector('.botao-facil'),
     medio: document.querySelector('.botao-medio'),
@@ -27,9 +35,42 @@ const elementos = {
 };
 
 const palavras = {
-  facil: [{valor:'anciã',dica:'ancia'}, {valor:'série',dica:'serie'}, {valor:'avaro',dica:'avaro'}, {valor:'maior',dica:'maior'}, {valor:'noite',dica:'noite'}, {valor:'ímpar',dica:'impar'}, {valor:'salvo',dica:'salvo'}, {valor:'vetor',dica:'vetor'}, {valor:'prado',dica:'prado'}, {valor:'pecha',dica:'pecha'}],
-  medio: [{valor:'cônjuge',dica:''}, {valor:'exceção',dica:''}, {valor:'efêmero',dica:''}, {valor:'prolixo',dica:''}, {valor:'idílico',dica:''}, {valor:'análogo',dica:''}, {valor:'caráter',dica:''}, {valor:'genuíno',dica:''}, {valor:'estória',dica:''}, {valor:'sublime',dica:''}],
-  dificil: [{valor:'concepção',dica:''}, {valor:'´plenitude',dica:''}, {valor:'essencial',dica:''}, {valor:'hipócrita',dica:''}, {valor:'corolário',dica:''}, {valor:'paradigma',dica:''}, {valor:'dicotomia',dica:''}, {valor:'hegemonia',dica:''}, {valor:'ratificar',dica:''}, {valor:'propósito',dica:''}]
+  facil: [
+    { valor: 'anciã', dica: 'Mulher de idade avançada, antigo, velho, respeitável' },
+    { valor: 'série', dica: 'Repetições de exercícios físicos' },
+    { valor: 'avaro', dica: 'Zelo/ciúme' },
+    { valor: 'maior', dica: 'Menor' },
+    { valor: 'noite', dica: 'A _____ é uma criança' },
+    { valor: 'ímpar', dica: 'Triângulo tem lados _____' },
+    { valor: 'salvo', dica: 'Ñ _____' },
+    { valor: 'vetor', dica: '...são segmentos de retas usados para representar alguma grandeza' },
+    { valor: 'prado', dica: 'substantivo masculino Terreno coberto de plantas herbáceas que servem para pastagem; campina.' },
+    { valor: 'pecha', dica: 'tem a _____ de mentiroso' },
+  ],
+  medio: [
+    { valor: 'cônjuge', dica: 'meu _______ que me pediu em casamento' },
+    { valor: 'exceção', dica: 'aquele que se desvia ou exclui de regras e padrões' },
+    { valor: 'efêmero', dica: 'que é passageiro, temporário, transitório' },
+    { valor: 'prolixo', dica: 'cansativo por estender-se demais no tempo; que tende a arrastar-se' },
+    { valor: 'idílico', dica: 'que é maravilhoso, ideal' },
+    { valor: 'análogo', dica: 'de função semelhante mas de origem embriologicamente distinta (diz-se de órgão em relação a outro)' },
+    { valor: 'caráter', dica: 'define a índole da pessoa e como ela rege as suas atitudes' },
+    { valor: 'genuíno', dica: 'próprio, exato, legítimo, verdadeiro' },
+    { valor: 'estória', dica: 'narrativa de cunho popular e tradicional' },
+    { valor: 'sublime', dica: 'superlativamente belo, esteticamente perfeito; grandioso, soberbo' },
+  ],
+  dificil: [
+    { valor: 'concepção', dica: 'ação ou efeito de gerar (ou ser gerado) um ser vivo' },
+    { valor: '´plenitude', dica: 'estado do que é inteiro, completo; totalidade, integridade' },
+    { valor: 'essencial', dica: 'que constitui o mais básico ou o mais importante em algo' },
+    { valor: 'hipócrita', dica: 'é o ato de fingir ter crenças, virtudes, ideias e sentimentos que a pessoa na verdade não possui' },
+    { valor: 'corolário', dica: 'é uma afirmação deduzida de uma verdade já demonstrada' },
+    { valor: 'paradigma', dica: 'um exemplo que serve como modelo; padrão' },
+    { valor: 'dicotomia', dica: 'aspecto que tem um planeta ou um satélite ao se apresentar dividido ao meio, metade claro, metade escuro' },
+    { valor: 'hegemonia', dica: 'autoridade soberana; liderança, predominância ou superioridade' },
+    { valor: 'ratificar', dica: 'patentear a verdade de; comprovar, corroborar' },
+    { valor: 'propósito', dica: 'aquilo que se busca alcançar; objetivo, finalidade, intuito' },
+  ],
 };
 
 const novoJogo = () => {
@@ -39,7 +80,7 @@ const novoJogo = () => {
       original: undefined,
       semAcentos: undefined,
       tamanho: undefined,
-      dica:undefined,
+      dica: undefined,
     },
     acertos: undefined,
     jogadas: [],
@@ -47,7 +88,7 @@ const novoJogo = () => {
     definirPalavra: function (palavra) {
       this.palavra.original = palavra.valor;
       this.palavra.tamanho = palavra.valor.length;
-      this.palavra.dica = palavra.dica
+      this.palavra.dica = palavra.dica;
       this.acertos = '';
       this.palavra.semAcentos = this.palavra.original.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
       for (let i = 0; i < this.palavra.tamanho; i++) {
@@ -164,17 +205,35 @@ const iniciarJogo = dificuldade => {
   mostrarPalavra();
 };
 
-const cadastrar = () =>{
+const abrirCadastro = () => {
   elementos.telaInicial.style.display = 'none';
-  elementos.telaCadastro.style.display = 'flex';
-}
+  elementos.telaCadastro.style.display = 'block';
+
+  elementos.formulario.facil.checked = false;
+  elementos.formulario.medio.checked = false;
+  elementos.formulario.dificil.checked = false;
+  elementos.formulario.palavra.textContent = '';
+  elementos.formulario.dica.textContent = '';
+
+  elementos.formulario.botaoCadastro.addEventListener('click', e => {
+    e.preventDefault();
+
+    if (elementos.formulario.facil.checked) {
+      console.log('facil');
+    } else if (elementos.formulario.medio.checked) {
+      console.log('medio');
+    } else if (elementos.formulario.dificil.checked) {
+      console.log('dificil');
+    }
+  });
+};
 
 const replace = (str, i, newChar) => str.substring(0, i) + newChar + str.substring(i + 1);
 
 elementos.botoes.facil.addEventListener('click', () => iniciarJogo('facil'));
 elementos.botoes.medio.addEventListener('click', () => iniciarJogo('medio'));
 elementos.botoes.dificil.addEventListener('click', () => iniciarJogo('dificil'));
-elementos.botoes.cadastrar.addEventListener('click', () =>cadastrar());
+elementos.botoes.cadastrar.addEventListener('click', () => abrirCadastro());
 
 elementos.botoes.reiniciar.addEventListener('click', () => novoJogo());
 
